da8bb54e02ba57e5764fa0d8c071a2901cb1ed54
Eivlisskiv <gautma13@gmail.com>
2023-09-14 04:08:46 -0400

    linqex;

---
+++ Collections/LinqEx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Collections
+{
+	public static class LinqEx
+	{
+		public static T WhereMin<T, N>(this IEnumerable<T> items, Func<T, N> getComparable)
+			where N : IComparable<N>
+		{
+			IEnumerator<T> enumerator = items.GetEnumerator();
+
+			bool first = true;
+			T target = default;
+			N min = default;
+			while (enumerator.MoveNext())
+			{
+				T value = enumerator.Current;
+				N other = getComparable(value);
+
+				if (first)
+					first = false;
+				else if (min.CompareTo(other) >= 0)
+					continue;
+				
+				target = value;
+				min = other;
+			}
+
+			return target;
+		}
+	}
+}
