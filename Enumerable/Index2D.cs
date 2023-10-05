c153887f23c7e91ce56c2bf8bd3ec000227e37bb
Eivlisskiv <gautma13@gmail.com>
2023-10-04 20:37:01 -0400

    index2d, indexed enum result

---
+++ Enumerable/Index2D.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Utilities.Enumerable
+{
+	public readonly struct Index2D<T>
+	{
+		public readonly int x;
+		public readonly int y;
+		public readonly T value;
+
+		public Index2D(int x, int y, T value)
+		{
+			this.x = x;
+			this.y = y;
+			this.value = value;
+		}
+	}
+
+	public static class EnumratorUtils
+	{
+		public static IEnumerator<Index2D<T>> Enumerate2D<T>(this T[,] array)
+		{
+			return new Enumerator2D<T>(array);
+		}
+	}
+
+	public class Enumerator2D<T> : IEnumerator<Index2D<T>>
+	{
+		int x = 0;
+		int y = -1;
+		private readonly int xSize;
+		private readonly int ySize;
+		private T[,] _values;
+
+		public Index2D<T> Current => new Index2D<T>(x, y, _values[x,y]);
+
+		object IEnumerator.Current => Current;
+
+		public Enumerator2D(T[,] values)
+		{
+			_values = values;
+			if (_values == null) return;
+			xSize = _values.GetLength(0);
+			ySize = _values.GetLength(1);
+		}
+
+		public bool MoveNext()
+		{
+			if (_values == null) return false;
+
+			if (y < ySize)
+			{
+				y++;
+				return true;
+			}
+
+			x++;
+			y = 0;
+			return x <= xSize;
+		}
+
+		public void Reset()
+		{
+			x = 0;
+			y = -1;
+		}
+
+		public void Dispose()
+		{
+			_values = null;
+		}
+	}
+}
