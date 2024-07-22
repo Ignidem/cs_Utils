3e73533fbaf596aab080da417689d12d7cfa1735
Eivlisskiv <gautma13@gmail.com>
2024-07-22 16:10:37 -0400

    async task utils

---
+++ Asyncronous/TaskUtils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Utils.Asyncronous
+{
+	public static class TaskUtils
+	{
+		public static Task OnSuccess(this Task task, Action onSuccess)
+		{
+			AwaitSuccess(task, onSuccess);
+			return task;
+		}
+		private static async void AwaitSuccess(this Task task, Action action)
+		{
+			if (task == null || action == null) return;
+
+			try
+			{
+				await task;
+				action();
+			}
+			catch { }
+		}
+
+		public static Task<T> OnSuccess<T>(this Task<T> task, Action<T> onSuccess)
+		{
+			AwaitSuccess(task, onSuccess);
+			return task;
+		}
+		private static async void AwaitSuccess<T>(this Task<T> task, Action<T> action)
+		{
+			if (task == null || action == null) return;
+
+			try
+			{
+				T result = await task;
+				action(result);
+			}
+			catch { }
+		}
+
+		public static Task OnFailure(this Task task, Action<Exception> onFailure)
+		{
+			AwaitFailure(task, onFailure);
+			return task;
+		}
+		private static async void AwaitFailure(this Task task, Action<Exception> action)
+		{
+			if (task == null || action == null) return;
+
+			try
+			{
+				await task;
+			}
+			catch (Exception e)
+			{
+				action(e);
+			}
+		}
+	}
+}
