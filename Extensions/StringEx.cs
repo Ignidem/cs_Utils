8daaad60895eaebf7a3df20b9e3136f998b3167a
Eivlisskiv <gautma13@gmail.com>
2024-09-09 18:45:49 -0400

    string to readable

---
+++ Extensions/StringEx.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Utilities.Extensions
+{
+	public static class StringEx
+	{
+		public static string ToReadable(this string text)
+		{
+			StringBuilder sb = new(text);
+
+			for (int i = 0; i < sb.Length; i++)
+			{
+				char c = sb[i];
+				switch (c)
+				{
+					case '_':
+						sb[i] = ' ';
+						continue;
+				}
+
+				if (i == 0)
+				{
+					sb[i] = char.ToUpper(c);
+				}
+				else
+				{
+					if (char.IsUpper(c) && char.IsLetter(sb[i-1]))
+					{
+						sb.Insert(i, ' ');
+						continue;
+					}
+				}
+			}
+
+			return sb.ToString();
+		}
+	}
+}
