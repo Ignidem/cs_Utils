fb97db8ad0b72f603b16ecbc2bae412fedc86547
Eivlisskiv <gautma13@gmail.com>
2025-03-06 16:15:48 -0500

    get flags with evaluated single bit flags

--- Enums/EnumUtils.cs
+++ Enums/EnumUtils.cs
@@ -5,11 +5,29 @@ namespace Utilities.Enums
 {
 	public static class EnumUtils
 	{
+		public static bool IsSingleBit<T>(T value)
+			where T : Enum
+		{
+			long val = Convert.ToInt64(value);
+			return val != 0 && (val & (val - 1)) == 0;
+		}
+		
 		public static T[] GetValues<T>() where T : Enum
 		{
 			return (T[])Enum.GetValues(typeof(T));
 		}
 
+		public static IEnumerable<T> GetFlags<T>(this T flags)
+			where T : Enum
+		{
+			T[] values = GetValues<T>();
+			foreach (T flag in Enum.GetValues(typeof(T)))
+			{
+				if (flags.HasFlag(flag) && IsSingleBit(flag))
+					yield return flag;
+			}
+		}
+		
 		public static IEnumerable<T> GetFlags<T>(this T flags, params T[] baseValues)
 			where T : Enum
 		{
