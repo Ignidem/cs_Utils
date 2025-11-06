cd528c285b13f085cd08a6b6e601aa1695bb65ab
Eivlisskiv <gautma13@gmail.com>
2025-11-06 15:06:30 -0500

    fixed missing usings

--- Serializers/WritableObjects/Writer/WritableDebugUtils.cs
+++ Serializers/WritableObjects/Writer/WritableDebugUtils.cs
@@ -1,57 +1,56 @@
-#define WritableDebugging
-#if WritableDebugging
+using System;
 using System.Collections;
 using System.Text;
 
-namespace Utils.Serializers.WritableObjects;
-
-public interface IWritableDebug
+namespace Utils.Serializers.WritableObjects
 {
-	StringBuilder DebugContent { get; }
-	int Indent { get; set; }
-}
-
-public static class WritableDebugUtils
-{
-	public static string GetIndent(this IWritableDebug debug)
+	public interface IWritableDebug
 	{
-		return new string('\t', debug.Indent);
+		StringBuilder DebugContent { get; }
+		int Indent { get; set; }
 	}
 
-	public static void StartValue(this IWritableDebug debug, Type type)
+	public static class WritableDebugUtils
 	{
-		debug.DebugContent.Append(debug.GetIndent());
-		if (type.IsValueType || type == typeof(string))
-			return;
-
-		debug.Indent++;
-		if (type.IsArray || typeof(IList).IsAssignableFrom(type))
-			debug.DebugContent.Append('[');
-		else
-			debug.DebugContent.AppendFormat("{0}: {{", type.Name);
-
-		debug.DebugContent.Append('\n');
-	}
-
-	public static void Value(this IWritableDebug debug, Type type, object value)
-	{
-		if (value == null || type.IsValueType || type == typeof(string))
+		public static string GetIndent(this IWritableDebug debug)
 		{
-			debug.DebugContent.AppendFormat("{0}: {1}\n", type, value);
+			return new string('\t', debug.Indent);
+		}
+
+		public static void StartValue(this IWritableDebug debug, Type type)
+		{
+			debug.DebugContent.Append(debug.GetIndent());
+			if (type.IsValueType || type == typeof(string))
+				return;
+
+			debug.Indent++;
+			if (type.IsArray || typeof(IList).IsAssignableFrom(type))
+				debug.DebugContent.Append('[');
+			else
+				debug.DebugContent.AppendFormat("{0}: {{", type.Name);
+
+			debug.DebugContent.Append('\n');
+		}
+
+		public static void Value(this IWritableDebug debug, Type type, object value)
+		{
+			if (value == null || type.IsValueType || type == typeof(string))
+			{
+				debug.DebugContent.AppendFormat("{0}: {1}\n", type, value);
+			}
+		}
+
+		public static void EndValue(this IWritableDebug debug, Type type)
+		{
+			if (type.IsValueType || type == typeof(string))
+				return;
+
+			debug.Indent--;
+
+			if (type.IsArray || typeof(IList).IsAssignableFrom(type))
+				debug.DebugContent.Append(debug.GetIndent()).Append("]\n");
+			else
+				debug.DebugContent.AppendFormat(debug.GetIndent()).Append("}\n");
 		}
 	}
-
-	public static void EndValue(this IWritableDebug debug, Type type)
-	{
-		if (type.IsValueType || type == typeof(string))
-			return;
-
-		debug.Indent--;
-
-		if (type.IsArray || typeof(IList).IsAssignableFrom(type))
-			debug.DebugContent.Append(debug.GetIndent()).Append("]\n");
-		else
-			debug.DebugContent.AppendFormat(debug.GetIndent()).Append("}\n");
-	}
 }
-#endif
